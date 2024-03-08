2116bec310da40f9587c049f6ee50bc613adebaa
gigantesampfc123 <162756747+gigantesampfc123@users.noreply.github.com>
2024-03-08 20:30:08 -0300

    Atualizar o RoleData.ts

--- src/src/types/RoleData.ts
+++ src/src/types/RoleData.ts
@@ -1,1 +1,9 @@
-.
+export interface RoleData {
+    name: string;
+    color: `#${string}`;
+    hoist: boolean;
+    permissions: string;
+    mentionable: boolean;
+    position: number;
+    isEveryone: boolean;
+}
